c56bae8835d30ff26f81848a449edb7f6e9bf560
Jaegoomon <jaegoo1199@gmail.com>
2021-08-21 14:46:21 +0900

    env 파일 세팅

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -12,15 +12,25 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# reading .env file
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'elgqwjfb6kv%nry73cvn@n#kx=r8s*c=s*r+p1xzq&=)wf5%&r'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
