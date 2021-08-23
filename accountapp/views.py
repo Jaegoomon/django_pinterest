fe236c05036845ab160fadd4d20dcf7575d4f7a3
Jaegoomon <jaegoo1199@gmail.com>
2021-08-23 11:04:03 +0900

    CreateView 적용

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.urls.base import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 from accountapp.models import HelloWorld
@@ -9,7 +13,6 @@ from accountapp.models import HelloWorld
 def hello_world(request):
 
     if request.method == "POST":
-
         temp = request.POST.get('hello_world_input')
 
         new_hello_world = HelloWorld()
@@ -21,3 +24,10 @@ def hello_world(request):
     else:
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/helloworld.html', context={'hello_world_list': hello_world_list})
+
+
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
