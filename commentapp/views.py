4ec50adef1052d84cbf11f33bf5abfcfe3c4c88d
Jaegoomon <jaegoo1199@gmail.com>
2021-08-25 21:52:55 +0900

    commentapp 구현

--- commentapp/views.py
+++ commentapp/views.py
@@ -1,3 +1,5 @@
+from commentapp.decorators import comment_ownership_required
+from django.views.generic.edit import DeleteView
 from articleapp.models import Article
 from commentapp.forms import CommentCreationForm
 from commentapp.models import Comment
@@ -7,6 +9,8 @@ from django.urls import reverse
 from django.http import HttpResponse
 from django.forms import ModelForm
 # Create your views here.
+
+from django.utils.decorators import method_decorator
 
 
 class CommentCreateView(CreateView):
@@ -21,6 +25,17 @@ class CommentCreateView(CreateView):
         temp_comment.writer = self.request.user
         temp_comment.save()
         return super().form_valid(form)
+
+    def get_success_url(self) -> str:
+        return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
+
+
+# @method_decorator(comment_ownership_required, 'get')
+# @method_decorator(comment_ownership_required, 'post')
+class CommentDeleteView(DeleteView):
+    model = Comment
+    context_object_name = "target_comment"
+    template_name = "commentapp/delete.html"
 
     def get_success_url(self) -> str:
         return reverse('articleapp:detail', kwargs={'pk': self.object.article.pk})
